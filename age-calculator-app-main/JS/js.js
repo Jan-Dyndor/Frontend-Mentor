ecb05e017de15a06cd84c443bfdd4cbbf7f5d820
Jan Dyndor <dyndorjan@gmail.com>
2023-06-06 23:18:19 +0200

    implementing JS

--- age-calculator-app-main/JS/js.js
+++ age-calculator-app-main/JS/js.js
@@ -1,2 +1,57 @@
 "use strict";
-element;
+
+// test
+const test = document.querySelector(".text");
+
+document.querySelector(".iconDIV").addEventListener("click", function () {
+  // inputs
+  const day = Number(document.querySelector(".dayInput").value);
+  const month = Number(document.querySelector(".monthInput").value);
+  const year = Number(document.querySelector(".yearInput").value);
+  // label
+  const label = document.getElementsByTagName("label");
+  // messages
+  const noNumberDay = document.querySelector(".messageNND");
+  const noNumberMonth = document.querySelector(".messageNNM");
+  const noNumberYear = document.querySelector(".messageNNY");
+  // Cheching if inputs fields are empty
+  if (!day || isNaN(day)) {
+    noNumberDay.style.display = "block";
+    noNumberDay.style.color = "red";
+    label[0].style.color = "red";
+    return;
+  }
+  if (!month || isNaN(month)) {
+    noNumberMonth.style.display = "block";
+    noNumberMonth.style.color = "red";
+    label[1].style.color = "red";
+    return;
+  }
+  if (!year || isNaN(year)) {
+    noNumberYear.style.display = "block";
+    noNumberYear.style.color = "red";
+    label[2].style.color = "red";
+    return;
+  }
+  // ! Check if putting there return is a good idea
+  // chech if number is valid
+  if (day < 1 || day > 31) {
+    noNumberDay.style.display = "block";
+    noNumberDay.style.color = "red";
+    label[0].style.color = "red";
+    return;
+  }
+  if (month < 1 || month > 31) {
+    noNumberDay.style.display = "block";
+    noNumberDay.style.color = "red";
+    label[0].style.color = "red";
+    return;
+  }
+  if (year < 1 || year > 31) {
+    noNumberDay.style.display = "block";
+    noNumberDay.style.color = "red";
+    label[0].style.color = "red";
+    return;
+  }
+  console.log(day);
+});
